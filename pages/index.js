4f998089ba81251c52b51335e73edaabef104c69
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2022-06-12 23:31:49 -0300

    1.1

--- pages/index.js
+++ pages/index.js
@@ -157,10 +157,10 @@ export default function Home(){
                     <p style={stylesheet.resultado}>FGTS: </p>
                     </Col>
                     <Col>
-                    <p style={stylesheet.resultado}>{salarioLiquido}</p>
-                    <p style={stylesheet.resultado}>{INSS}</p>
-                    <p style={stylesheet.resultado}>{IRPF}</p>
-                    <p style={stylesheet.resultado}>{FGTS}</p>
+                    <p style={stylesheet.resultado}>R${salarioLiquido}</p>
+                    <p style={stylesheet.resultado}>R${INSS}</p>
+                    <p style={stylesheet.resultado}>R${IRPF}</p>
+                    <p style={stylesheet.resultado}>R${FGTS}</p>
                     </Col>
                     </Row>
                 </div>
